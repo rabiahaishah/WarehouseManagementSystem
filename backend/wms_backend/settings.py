53022adab4255859d331e91beb4341c15c9f9df2
rabiahaishah <aishahrabiah@gmail.com>
2025-07-07 18:37:13 +0800

    Fix: setup for Railway deployment

--- backend/wms_backend/settings.py
+++ backend/wms_backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']
 
 # Application definition
 
