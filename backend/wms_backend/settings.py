42c0bf54af5fb6b4ca9cd7f7277e45933b537225
Rabiah Aishah Ahmad <151909374+rabiahaishah@users.noreply.github.com>
2025-07-07 17:55:07 +0800

    Update settings.py

--- backend/wms_backend/settings.py
+++ backend/wms_backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['13.238.195.111', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 # Application definition
 
